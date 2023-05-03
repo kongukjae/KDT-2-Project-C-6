08cfe310ffb0fb52372c4bc5e91852899982a638
KangJeemin <wlals1234560@gmail.com>
2023-05-03 16:47:21 +0900

    mysql2/promise 방식으로 회원가입 및 중복확인 기능 하려고 하다가 promise 안 쓰고 원래 하던 방식에서, where을 사용해 length의 유무로 중복을 확인하려고 함.

--- join.js
+++ join.js
@@ -1,3 +1,4 @@
+
 let object = {
   id:"강지민1",
   password:"코드기어",
@@ -11,19 +12,33 @@ function join(object){
     port     : 3307,
     database : 'test'
   });
-  
-  connection.connect();
-  
-  connection.query(`insert into test.new_table(id_, password, name) values ('${object.id}', '${object.password}','${object.name}')`, function (error, results, fields) {
-    if (error) {
-      console.log("error")
-    };
-    console.log('The solution is: ', results);
+  connection.connect(err=>{
+    if(err){
+      console.log("DB와 연결중에 에러가 발생했습니다.");
+    }
+    else
+    console.log("DB와 연결 완료");
   });
+
+connection.query(`SELECT * FROM test.new_table WHERE id_ ="${object.id}"`,(error,results)=>{
+  if(error){
+    console.log("에러발생")
+  }
+  else{
+    console.log(results)
+  }
+})
   
-  connection.end();
+// connection.query(`insert into test.new_table(id_, password, name) values ('${object.id}', '${object.password}','${object.name}')`, function (error, results, fields) {
+//      if (error) {
+//        console.log("error")
+//      };
+//      console.log('The solution is: ', results);
+//    });
+  
+ connection.end();
 
 
-}
+ }
 
 join(object)
