1d860dd2421ffa04f7c0b8340ed5ecba098e9019
KangJeemin <wlals1234560@gmail.com>
2023-05-09 13:03:08 +0900

    로드하려는 방식은 모듈 스크립트인데, content-type을 어떻게 설정해야하지?

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 import http from 'http'
 import url from 'url'
 import fs from 'fs'
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); //es6에서는 __dirname 지원 X
 
 const server = http.createServer((req,res)=>{
-  const
+  const filePath = path.join(__dirname,'/layout1.html');
+  fs.readFile(filePath,'utf-8',(err,data)=>{
+    if(err){
+      console.log(err);
+      res.statusCode=500;
+      res.end('err')
+    }
+    else{
+      res.statusCode=200;
+      res.setHeader('Content-Type','text/javascript');
+      res.end(data)
+    }
+  })  
   
-  res.statusCode=200;
-  res.setHeader('Content-Type','text/html');
-  if(req.url=="/main"){
-    res.write("zz")
-    console.log(req)
-  }
-  res.end()
+  // if(req.url=="/main"){
+  //   res.write("zz")
+  //   console.log(req)
+  // }
+  // res.end()
 });
 
 server.listen(3000,()=>{
