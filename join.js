bd0eab09b8cf584fcbdc3459863f207018f206bc
KangJeemin <wlals1234560@gmail.com>
2023-05-10 17:43:36 +0900

    쿼리문에서 왜 에러가 발생?

--- join.js
+++ join.js
@@ -1,10 +1,14 @@
+import mysql from 'mysql2'
 let object = {
-  id:"wlals12345601",
+  userid:"wlals1234560123",
   password:"dD1354268",
-  name:"강지민"
+  name:"강지민",
+  number1:10,
+  number2:4929,
+  number3:7381,
+  email:"naver.com"
 }
 function join(object){
-  let mysql = require('mysql2');
   let connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
@@ -15,22 +19,28 @@ function join(object){
   connection.connect(err=>{
     if(err){
       console.log("DB와 연결중에 에러가 발생했습니다.");
+    connection.end();
+
     }
     else
     console.log("DB와 연결 완료");
   });
 
-connection.query(`SELECT * FROM test.new_table WHERE id_ ="${object.id}"`,(error,results)=>{
+connection.query(`SELECT * FROM test.new_table WHERE userid ="${object.userid}"`,(error,results)=>{
   if(error){
     console.log("에러발생")
+    connection.end();
+
   }
   else{
     if(results.length>=1){
       console.log("아이디 중복 발생")
+      connection.end();
+
       return false;
     }
     else{
-      connection.query(`insert into test.new_table(id_, password, name) values ('${object.id}', '${object.password}','${object.name}')`, function (error, results1, fields) {
+      connection.query(`insert into test.new_table(userid, password, name, number1, number2, number3, email) values ('${object.userid}', '${object.password}','${object.name}','${object.number1}', '${object.number2}','${object.number3}','${object.email}')`, function (error, results1, fields) {
         if (error) {
           console.log("error")
           return false;
@@ -46,15 +56,6 @@ connection.query(`SELECT * FROM test.new_table WHERE id_ ="${object.id}"`,(error
   }
 })
 connection.end();
-<<<<<<< HEAD
-  
-  
-
-
-
- }
+}
 
 join(object)
-=======
-}
->>>>>>> main
