1f8fb50830509176fe8c2e3f98dd3ac41ff10bd2
KangJeemin <wlals1234560@gmail.com>
2023-05-11 12:43:07 +0900

    <토큰 문제 해결, 코드상 스코프의 문제였음.

--- server.js
+++ server.js
@@ -1,58 +1,52 @@
 import http from 'http'
-import url from 'url'
 import fs from 'fs'
-import path from "path";
 
 const server = http.createServer((req,res)=>{
   // const url = url.parse(req.url);
-  if(req.method=="GET"){ 
-    fs.readFile('./layout1.html','utf-8',(err,data)=>{
-      if(err){
-        console.log(err);
-        res.statusCode=500;
-        res.end('err')
-      }
-      else{
-        res.writeHead(200,'Content-Type','text/html');
-        console.log(req.url)
-        res.write(data)
-        res.end()
-
-        if(req.url==='/core.js'){
-          fs.readFileSync('./core.js','utf-8',(err,data1)=>{
-            if(err){
-              throw new Error
-            }
-            else{
-            
-            res.writeHead(200,'Content-Type','text/javascript');
-            res.end(data1);
-            }
-          })
-        }
-        else if(req.url==='/TopLayout.js'){
-          fs.readFileSync('./TopLayout.js','utf-8',(err,data2)=>{
-            if(err){
-              console.log(err)
-            }
-            else{
-            res.writeHead(200,'Content-Type','text/javascript');
-            res.end(data2);
-            }
-          })
+  if(req.method=="GET"){
+    console.log(req.url) 
+    if(req.url==='/'){
+      fs.readFile('./layout1.html','utf-8',(err,data)=>{
+        if(err){
+          console.log(err);
+          res.statusCode=500;
+          res.end('err')
         }
         else{
-          console.log(data)
-
-          
+          res.writeHead(200,'Content-Type','text/html');
+          res.write(data)
+          res.end()
         }
-      }
-    
     })
+    }
+    if(req.url==='/core.js'){
+      fs.readFile('./core.js','utf-8',(err,data1)=>{
+        if(err){
+          console.log(err);
+          res.statusCode=500;
+          res.end('err')
+        }
+        else{
+          res.writeHead(200,'Content-Type','text/javascript');
+          res.end(data1);
+        }
+      })
+    }
+    if(req.url==='/TopLayout.js'){
+      fs.readFile('./TopLayout.js','utf-8',(err,data2)=>{
+        if(err){
+          console.log(err)
+        }
+        else{
+        res.writeHead(200,'Content-Type','text/javascript');
+        res.end(data2);
+        }
+      })
+    }
+  }}
+  )
 
-  }
-});
 
 server.listen(3000,()=>{
-  console.log("서버가동")
+  console.log("서버가동 http://localhost:3000")
 })
