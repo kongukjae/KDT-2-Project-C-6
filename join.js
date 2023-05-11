839dcd116723307343de982e1b1a596731950fb6
KangJeemin <wlals1234560@gmail.com>
2023-05-11 11:54:16 +0900

    연결 오류 때문에 createconnection -> createPool 로 함수 교체

--- join.js
+++ join.js
@@ -3,24 +3,22 @@ let object = {
   userid:"wlals1234560123",
   password:"dD1354268",
   name:"강지민",
-  number1:10,
-  number2:4929,
-  number3:7381,
+  number1:"10",
+  number2:"4929",
+  number3:"7381",
   email:"naver.com"
 }
 function join(object){
-  let connection = mysql.createConnection({
+  let connection = mysql.createPool( {
     host     : 'localhost',
     user     : 'root',
     password : 'dD1354268!!',
     port     : 3307,
     database : 'test'
   });
-  connection.connect(err=>{
+  connection.getConnection(err=>{
     if(err){
-      console.log("DB와 연결중에 에러가 발생했습니다.");
-    // connection.end();
-
+      console.log(err);
     }
     else
     console.log("DB와 연결 완료");
@@ -29,33 +27,26 @@ function join(object){
 connection.query(`SELECT * FROM test.new_table WHERE userid ="${object.userid}"`,(error,results)=>{
   if(error){
     console.log("에러발생")
-    // connection.end();
   }
   else{
     if(results.length>=1){
       console.log("아이디 중복 발생")
-      // connection.end();
-      return false;
     }
     else{
       connection.query(`insert into test.new_table(userid, password, name, number1, number2, number3, email) values ('
-      ${object.userid}', '${object.password}','${object.name}',${object.number1}, ${object.number2},${object.number3},'${object.email}
+      ${object.userid}', '${object.password}','${object.name}','${object.number1}', '${object.number2}','${object.number3}','${object.email}
       ')`, function (error, results1, fields) {
         if (error) {
           console.log(error)
-          return false;
         }
         else{
           console.log('회원가입 완료:', results1);
-          connection.end();
-          return true;
-          
         }
       });      
     }
   }
+  connection.release();
 })
-connection.end();
 }
 
 join(object)
