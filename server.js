891d921e17f8e87c379dc67dd47507b809ad2d64
KangJeemin <wlals1234560@gmail.com>
2023-05-11 16:43:17 +0900

    /로 접속했을때, layout1.html 과 참조하고 있는 core.js, Toplayout.js 읽기

--- server.js
+++ server.js
@@ -2,49 +2,46 @@ import http from 'http'
 import fs from 'fs'
 
 const server = http.createServer((req,res)=>{
-  // const url = url.parse(req.url);
-  if(req.method=="GET"){
-    console.log(req.url) 
-    if(req.url==='/'){
-      fs.readFile('./layout1.html','utf-8',(err,data)=>{
+  if(req.method==="GET" && req.url==="/"){
+    fs.readFile('./layout1.html','utf-8',(err,data)=>{
+      if(err){
+        console.log(err);
+        res.statusCode=500;
+        res.end('err')
+      }
+      else{
+        res.writeHead(200,'Content-Type','text/html');
+        res.end(data)
+      }
+    })
+    if(req.url==="/core.js"){
+      fs.readFile('./core.js','utf-8',(err,data)=>{
         if(err){
           console.log(err);
           res.statusCode=500;
           res.end('err')
         }
         else{
-          res.writeHead(200,'Content-Type','text/html');
+          res.writeHead(200,'Content-Type','text/javascript');
           res.end(data)
         }
       })
-        fs.readFile('./core.js','utf-8',(err,data1)=>{
-          if(err){
-            console.log(err);
-            res.statusCode=500;
-            res.end('err')
-          }
-          else{
-            res.writeHead(200,'Content-Type','text/javascript');
-            res.end(data1);
-          }
-        })
-          fs.readFile('./TopLayout.js','utf-8',(err,data2)=>{
-            if(err){
-              console.log(err)
-            }
-            else{
-            res.writeHead(200,'Content-Type','text/javascript');
-            res.end(data2);
-            }
-          })
     }
-    
-    
-   }
+    if(req.url==="/TopLayout.js"){
+      fs.readFile('./TopLayout.js','utf-8',(err,data)=>{
+        if(err){
+          console.log(err);
+          res.statusCode=500;
+          res.end('err')
+        }
+        else{
+          res.writeHead(200,'Content-Type','text/javascript');
+          res.end(data)
+        }
+      })
+    }
+  }
   })
-   
-
-
-server.listen(3000,()=>{
-  console.log("서버가동 http://localhost:3000")
-})
+server.listen(3001,()=>{
+  console.log("서버가동 http://localhost:3001");
+  })
