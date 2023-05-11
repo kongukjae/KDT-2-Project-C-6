bee852c56cdbd507bc1017efe65846561f90873a
KangJeemin <wlals1234560@gmail.com>
2023-05-11 11:55:54 +0900

    gpt로 문법적 오류 수정

--- join.js
+++ join.js
@@ -1,52 +1,52 @@
 import mysql from 'mysql2'
+
 let object = {
-  userid:"wlals1234560123",
-  password:"dD1354268",
-  name:"강지민",
-  number1:"10",
-  number2:"4929",
-  number3:"7381",
-  email:"naver.com"
+  userid: "wlals1234560123",
+  password: "dD1354268",
+  name: "강지민",
+  number1: "10",
+  number2: "4929",
+  number3: "7381",
+  email: "naver.com"
 }
-function join(object){
-  let connection = mysql.createPool( {
-    host     : 'localhost',
-    user     : 'root',
-    password : 'dD1354268!!',
-    port     : 3307,
-    database : 'test'
-  });
-  connection.getConnection(err=>{
-    if(err){
-      console.log(err);
-    }
-    else
-    console.log("DB와 연결 완료");
+
+function join(object) {
+  let connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'dD1354268!!',
+    port: 3307,
+    database: 'test'
   });
 
-connection.query(`SELECT * FROM test.new_table WHERE userid ="${object.userid}"`,(error,results)=>{
-  if(error){
-    console.log("에러발생")
-  }
-  else{
-    if(results.length>=1){
-      console.log("아이디 중복 발생")
-    }
-    else{
-      connection.query(`insert into test.new_table(userid, password, name, number1, number2, number3, email) values ('
-      ${object.userid}', '${object.password}','${object.name}','${object.number1}', '${object.number2}','${object.number3}','${object.email}
-      ')`, function (error, results1, fields) {
+  connection.getConnection((err, conn) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("DB와 연결 완료");
+
+      conn.query(`SELECT * FROM test.new_table WHERE userid ="${object.userid}"`, (error, results) => {
         if (error) {
-          console.log(error)
+          console.log("에러발생")
+        } else {
+          if (results.length >= 1) {
+            console.log("아이디 중복 발생")
+          } else {
+            conn.query(`INSERT INTO test.new_table(userid, password, name, number1, number2, number3, email) VALUES (
+              '${object.userid}', '${object.password}','${object.name}','${object.number1}', '${object.number2}','${object.number3}','${object.email}'
+            )`, function (error, results1, fields) {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log('회원가입 완료:', results1);
+              }
+            });
+          }
         }
-        else{
-          console.log('회원가입 완료:', results1);
-        }
-      });      
+        conn.release();
+      })
     }
-  }
-  connection.release();
-})
+  });
 }
 
 join(object)
