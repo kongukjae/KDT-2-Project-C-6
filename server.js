92e2b8ff8d1a2928ab2f83da5f721599f527db57
KangJeemin <wlals1234560@gmail.com>
2023-05-11 14:30:43 +0900

    html 먼저 읽어야하는데

--- server.js
+++ server.js
@@ -14,37 +14,35 @@ const server = http.createServer((req,res)=>{
         }
         else{
           res.writeHead(200,'Content-Type','text/html');
-          res.write(data)
-          res.end()
-        }
-    })
-    }
-    if(req.url==='/core.js'){
-      fs.readFile('./core.js','utf-8',(err,data1)=>{
-        if(err){
-          console.log(err);
-          res.statusCode=500;
-          res.end('err')
-        }
-        else{
-          res.writeHead(200,'Content-Type','text/javascript');
-          res.end(data1);
+          res.end(data)
         }
       })
+        fs.readFile('./core.js','utf-8',(err,data1)=>{
+          if(err){
+            console.log(err);
+            res.statusCode=500;
+            res.end('err')
+          }
+          else{
+            res.writeHead(200,'Content-Type','text/javascript');
+            res.end(data1);
+          }
+        })
+          fs.readFile('./TopLayout.js','utf-8',(err,data2)=>{
+            if(err){
+              console.log(err)
+            }
+            else{
+            res.writeHead(200,'Content-Type','text/javascript');
+            res.end(data2);
+            }
+          })
     }
-    if(req.url==='/TopLayout.js'){
-      fs.readFile('./TopLayout.js','utf-8',(err,data2)=>{
-        if(err){
-          console.log(err)
-        }
-        else{
-        res.writeHead(200,'Content-Type','text/javascript');
-        res.end(data2);
-        }
-      })
-    }
-  }}
-  )
+    
+    
+   }
+  })
+   
 
 
 server.listen(3000,()=>{
