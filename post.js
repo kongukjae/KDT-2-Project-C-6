1d44d53105821bc861b951a7d685e7d0be2613ca
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-05-23 22:19:14 +0900

    왜 서버 렌더링 안됨?

--- post.js
+++ post.js
@@ -1,38 +1,41 @@
-import http, { Server } from 'http'
+import http from 'http'
 
 const server = http.createServer((res,req)=>{
     if(req.method==="GET" && req.url==="/"){
+        res.writeHead(200,'Content-Type','text/plain')
+        res.write(firestPage)
+        res.end()
         let firestPage=`
         <!DOCTYPE html>
         <html lang="en">
         <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
         </head>
         <body>
-        <form method="POST" action="/a">
-        <input type="text" name="id">
-        <input type="submit">
-        </form>
+            <form method="post" action="/a">
+                <input type="text" name="id">
+                <input type="text" name="password">
+                <input type="submit">
+            </form>
         </body>
         </html>`
-        res.writeHead(200,'Content-Type','text/html')
-        res.end(firestPage)
+        
     }
     
-    let data=''
-    if(req.method==="POST" && req.url==="/a"){
-        req.on('data',(chunk)=>{
-            data+=chunk
-        })
-        req.on('end',()=>{
-            console.log(data);
-            res.writeHead(200,'text/plain')
-            res.end(data)
-        })
-    }
+    // let data=''
+    // if(req.method==="POST" && req.url==="/a"){
+    //     req.on('data',(chunk)=>{
+    //         data+=chunk
+    //     })
+    //     req.on('end',()=>{
+    //         console.log(data);
+    //         res.writeHead(200,'text/plain')
+    //         res.end(data)
+    //     })
+    // }
 
 })
 
@@ -40,54 +43,3 @@ server.listen(3000,()=>{
     console.log("http://localhost:3000/")
 })
 
-// const http = require('http');
-
-// const formTag = `
-// <form method="GET" action="/login">
-// <input type="text" name="id">
-// <input type="submit">
-// </form>
-// `;
-// function greet(fromSubmitString) {
-// return `<h1>${fromSubmitString}</h1>`;
-// }
-// function firstPage(data) {
-// return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-// <meta charset="UTF-8">
-// <meta http-equiv="X-UA-Compatible" content="IE=edge">
-// <meta name="viewport" content="width=device-width, initial-scale=1.0">
-// <title>Document</title>
-// </head>
-// <body>
-// ${data}
-// </body>
-// </html>
-// `;
-// }
-
-// const server = http.createServer(function(request, response){
-// // 최초접속
-// if(request.method === 'GET' && request.url === '/') {
-// response.writeHead(200, {'Content-Type': 'text/html'});
-// let page = firstPage(formTag);
-// response.write(page);
-// response.end();
-// }
-// // 무언가
-// if(request.method === 'GET' && request.url.startsWith('/login')) {
-// console.log(request.url);
-// const name = request.url.split('=')[1];
-// console.log(name);
-// response.writeHead(200, {'Content-Type': 'text/html'});
-// let page = firstPage(greet(name))
-// response.write(page);
-// response.end();
-// }
-// });
-// // 서버 포트 설정
-// server.listen(2080, function(error) {
-// if(error) { console.error('서버 안돌아감') } else { console.log('서버 돌아감'); }
-// });
