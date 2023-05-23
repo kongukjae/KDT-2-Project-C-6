d486dab01970b9053cab423f99f866294a764523
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-05-23 22:21:13 +0900

    res,req 순서 바꿔씀

--- post.js
+++ post.js
@@ -1,27 +1,27 @@
 import http from 'http'
 
-const server = http.createServer((res,req)=>{
+const server = http.createServer((req,res)=>{
     if(req.method==="GET" && req.url==="/"){
         res.writeHead(200,'Content-Type','text/plain')
-        res.write(firestPage)
-        res.end()
         let firestPage=`
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
         </head>
         <body>
-            <form method="post" action="/a">
-                <input type="text" name="id">
-                <input type="text" name="password">
-                <input type="submit">
-            </form>
+        <form method="post" action="/a">
+        <input type="text" name="id">
+        <input type="text" name="password">
+        <input type="submit">
+        </form>
         </body>
         </html>`
+        res.write()
+        res.end()
         
     }
     
