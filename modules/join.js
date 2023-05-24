2c60f8155a75d7cc1823a1e8b0f3f619141f1f6b
KangJeemin <wlals1234560@gmail.com>
2023-05-24 11:29:18 +0900

    전에 쓰던 로직 현재 테이블 형식에 맞게 수정

--- modules/join.js
+++ modules/join.js
@@ -1,58 +1,52 @@
-import mysql from mysql2
+import mysql from 'mysql2'
 
 let object = {
-  id:"wlals12345601",
-  password:"dD1354268",
-  name:"강지민",
-  phone1:"10",
-  phone2:"4929",
-  phone3:"7381"
+  id: "wlals1234560123",
+  password: "dD1354268",
+  name: "강지민",
+  phon1: "10",
+  phone2: "4929",
+  phone3: "7381",
+  email: "naver.com"
 }
 
-function join(object){
-  let mysql = require('mysql');
+function join(object) {
   let connection = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'dD1354268!',
-    port     : 3306,
-    database : 'gymwear'
+    host: 'localhost',
+    user: 'root',
+    password: 'dD1354268!',
+    port: 3306,
+    database: 'gymwear'
   });
-  connection.getConnection((err,conn)=>{
-    if(err){
-      console.log("DB와 연결중에 에러가 발생했습니다.");
-    }
-    else{
-    console.log("DB와 연결 완료");
-    
-    conn.query(`SELECT * FROM gymwear.new_table WHERE id ="${object.id}"`,(error,results)=>{
-      if(error){
-        console.log("에러발생")
-      }
-      else{
-        if(results.length>=1){
-          console.log("아이디 중복 발생")
-          return false;
-        }   
-        else{
-          conn.query(`insert into gymwear.new_table(id, password, name, phone1, phone2, phone3, email) values ('${object.id}', '${object.password}','${object.name}','${object.phone1}','${object.phone2}','${object.phone3}')`, function (error, results1, fields) {
-            if (error) {
-              console.log("error")
-              return false;
-            }
-            else{
-              console.log('회원가입 완료:', results1);
-              connection.end();
-              return true;
-            }
-          });      
+
+  connection.getConnection((err, conn) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("DB와 연결 완료");
+
+      conn.query(`SELECT * FROM gymwear.new_table WHERE id ="${object.id}"`, (error, results) => {
+        if (error) {
+          console.log("에러발생")
+        } else {
+          if (results.length >= 1) {
+            console.log("아이디 중복 발생")
+          } else {
+            conn.query(`INSERT INTO gymwear.new_table(id, password, name, phone1, phone2, phone3, email) VALUES (
+              '${object.id}', '${object.password}','${object.name}','${object.phon1}', '${object.phone2}','${object.phone3}','${object.email}'
+            )`, function (error, results1, fields) {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log('회원가입 완료:', results1);
+              }
+            });
+          }
         }
-        // conn.release();
-      }
-      conn.release();
-    })
-  }
-})
+        conn.release();
+      })
+    }
+  });
 }
 
 join(object)
