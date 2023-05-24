64301a0c95f43b409b4d77c811f69aee976657c4
KangJeemin <wlals1234560@gmail.com>
2023-05-24 11:54:07 +0900

    서버에서 정보 주고 이걸 db에 전달해주자

--- modules/join.js
+++ modules/join.js
@@ -21,13 +21,13 @@ function join(object) {
 
   connection.getConnection((err, conn) => {
     if (err) {
-      console.log(err);
+      console.err("DB연결 에러");
     } else {
       console.log("DB와 연결 완료");
 
       conn.query(`SELECT * FROM gymwear.new_table WHERE id ="${object.id}"`, (error, results) => {
         if (error) {
-          console.log("에러발생")
+          console.err("에러발생")
         } else {
           if (results.length >= 1) {
             console.log("아이디 중복 발생")
@@ -36,7 +36,7 @@ function join(object) {
               '${object.id}', '${object.password}','${object.name}','${object.phon1}', '${object.phone2}','${object.phone3}','${object.email}'
             )`, function (error, results1, fields) {
               if (error) {
-                console.log(error)
+                console.err("데이터베이스에 데이터 쓰기 오류");
               } else {
                 console.log('회원가입 완료:', results1);
               }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import http from 'http'
 import fs, { rmSync } from 'fs'
+import { join } from 'path';
 
 const server = http.createServer((req,res)=>{
   // const url = url.parse(req.url);
@@ -137,6 +138,17 @@ const server = http.createServer((req,res)=>{
       }
     })
   }
+  if(req.url==="/modules/join.js"){
+    fs.readFile('./modules/join.js','utf-8',(err,data)=>{
+      if(err){
+        console.err("에러발생")
+      }
+      else{
+        res.writeHead(200,'Content-Type','text/javascript')
+        res.end(data)
+      }
+    })
+  }
   if(req.url==="/views/js/joinLayout.js"){
     fs.readFile('./views/js/joinLayout.js','utf-8',(err,data)=>{
       if(err){
@@ -151,7 +163,7 @@ const server = http.createServer((req,res)=>{
   if(req.method==="POST" && req.url==="/a"){
     let data
     req.on('data',(chunk)=>{
-      data+=chunk
+      data+=chunk      
   })
   req.on('end',()=>{
       console.log(data);
