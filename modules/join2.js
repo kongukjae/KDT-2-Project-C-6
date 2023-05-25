b2bbdaef8169fb222b7ea50d7684cde167d7276b
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-05-26 00:10:57 +0900

    mysql에 데이터 쓰는 함수 만듦

---
+++ modules/join2.js
@@ -0,0 +1,27 @@
+import mysql from 'mysql2'
+function join2(object) {
+    new Promise((resolve,reject)=>{
+        let connection = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: 'dD1354268!',
+            port: 3306,
+            database: 'gymwear'
+          });
+          connection.getConnection((err,conn)=>{
+            conn.query(`INSERT INTO gymwear.new_table(id, password, name, phone1, phone2, phone3, email) VALUES (
+                      '${object.id}', '${object.password}','${object.name}','${object.phon1}', '${object.phone2}','${object.phone3}','${object.email}'
+                    )`, function (error, results1, fields) {
+                      if (error) {
+                        console.error("데이터베이스에 데이터 쓰기 오류");
+                        reject(error);
+                      } else {
+                        console.log('회원가입 완료:', results1);
+                        resolve(true)
+                      }
+                    });
+              conn.release();
+          })
+    })
+    }
+    export default join2
