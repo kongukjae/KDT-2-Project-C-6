a631dc1fea15ad944171d5f791132dafddcc2d34
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-06-13 22:25:43 +0900

    ★ 중요 해결 : webpack 설정에서 client 부분 번들링과 server 부분 번들링을 구분하여 설정함

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
+onst path = require('path');
 
-module.exports = {
+const clientConfig = {
   entry: './build/src/index.tsx',
   mode: 'production',
   module: {
@@ -25,7 +25,30 @@ module.exports = {
     extensions: ['.tsx','.ts','.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'client.bundle.js',
     path: path.resolve(__dirname, './dist'),
   },
+
 };
+const serverConfig = {
+  entry: './build/src/server/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'server.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+};
+module.exports = [clientConfig, serverConfig];
